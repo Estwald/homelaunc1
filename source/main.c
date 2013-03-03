f6d7545b227245407e7698c6dc81a69032679efe
Estwald <hermes.ps2@gmail.com>
2013-03-03 19:53:28 +0100

    New homelaunc1 for new syscall 8 and Iris Manager v2.20

--- source/main.c
+++ source/main.c
@@ -65,128 +65,29 @@ static u64 peekq(u64 addr)
     return_to_user_prog(u64);
 }
 
-#define NEW_POKE_SYSCALL                813
-
-#define SYSCALL_BASE_341                    0x80000000002EB128ULL
-#define NEW_POKE_SYSCALL_ADDR_341           0x80000000001BB93CULL   // where above syscall is in lv2
-#define SYSCALL_BASE_355                    0x8000000000346570ULL
-#define NEW_POKE_SYSCALL_ADDR_355           0x8000000000195A68ULL   // where above syscall is in lv2
-#define SYSCALL_BASE_355DEX                 0x8000000000361578ULL
-#define NEW_POKE_SYSCALL_ADDR_355DEX        0x800000000019be24ULL   // where above syscall is in lv2
-#define SYSCALL_BASE_421                    0x800000000035BCA8ULL
-#define NEW_POKE_SYSCALL_ADDR_421           0x80000000001B65C8ULL   // where above syscall is in lv2
-#define SYSCALL_BASE_421DEX                 0x800000000037A1B0ULL
-#define NEW_POKE_SYSCALL_ADDR_421DEX        0x80000000001BC9B8ULL   // where above syscall is in lv2
-#define SYSCALL_BASE_430                    0x800000000035DBE0ULL
-#define NEW_POKE_SYSCALL_ADDR_430           0x80000000001B6950ULL   // where above syscall is in lv2
-#define SYSCALL_BASE_431                    0x800000000035DBE0ULL
-#define NEW_POKE_SYSCALL_ADDR_431           0x80000000001B6958ULL   // where above syscall is in lv2
-
-u64 SYSCALL_BASE;
-
-int is_firm_341(void)
+static void pokeq(u64 addr, u64 data)
 {
-    u64 addr = peekq((SYSCALL_BASE_341 + NEW_POKE_SYSCALL * 8));
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0) return 0;
-    addr = peekq(addr);
-
-    SYSCALL_BASE = SYSCALL_BASE_341;
-    if(addr == NEW_POKE_SYSCALL_ADDR_341) return 1;
-    
-
-    return 0;
+    lv2syscall2(7, addr, data);
 }
 
-int is_firm_355(void)
-{
-    u64 addr = peekq((SYSCALL_BASE_355 + NEW_POKE_SYSCALL * 8));
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0) return 0;
-    addr = peekq(addr);
-
-    SYSCALL_BASE = SYSCALL_BASE_355;
-    if(addr == NEW_POKE_SYSCALL_ADDR_355) return 1;
-    
-
-    return 0;
-}
-
-int is_firm_355DEX(void)
-{
-    u64 addr = peekq((SYSCALL_BASE_355DEX + NEW_POKE_SYSCALL * 8));
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0) return 0;
-    addr = peekq(addr);
-
-    SYSCALL_BASE = SYSCALL_BASE_355DEX;
-    if(addr == NEW_POKE_SYSCALL_ADDR_355DEX) return 1;
-    
-
-    return 0;
-}
-
-int is_firm_421(void)
-{
-    u64 addr = peekq((SYSCALL_BASE_421 + NEW_POKE_SYSCALL * 8));
-    // check address first
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0)
-        return 0;
-    addr = peekq(addr);
-    
-    SYSCALL_BASE = SYSCALL_BASE_421;
-    if(addr == NEW_POKE_SYSCALL_ADDR_421) return 1;
-
-    return 0;
-}
-
-int is_firm_421DEX(void)
-{
-    u64 addr = peekq((SYSCALL_BASE_421DEX + NEW_POKE_SYSCALL * 8));
-    // check address first
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0)
-        return 0;
-    addr = peekq(addr);
-    
-    SYSCALL_BASE = SYSCALL_BASE_421DEX;
-    if(addr == NEW_POKE_SYSCALL_ADDR_421DEX) return 1;
-
-    return 0;
-}
-
-int is_firm_430(void)
-{
-    u64 addr = peekq((SYSCALL_BASE_430 + NEW_POKE_SYSCALL * 8));
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0) return 0;
-    addr = peekq(addr);
-
-    SYSCALL_BASE = SYSCALL_BASE_430;
-    if(addr == NEW_POKE_SYSCALL_ADDR_430) return 1;
-   
-    return 0;
-}
-
-int is_firm_431(void)
-{
-    u64 addr = peekq((SYSCALL_BASE_431 + NEW_POKE_SYSCALL * 8));
-    if(addr < 0x8000000000000000ULL || addr > 0x80000000007FFFFFULL || (addr & 3)!=0) return 0;
-    addr = peekq(addr);
-
-    SYSCALL_BASE = SYSCALL_BASE_431;
-    if(addr == NEW_POKE_SYSCALL_ADDR_431) return 1;
-   
-    return 0;
-}
+u64 restore_syscall8[2]= {0,0};
 
 int is_payload_loaded(void)
 {
-    if(!is_firm_341() && !is_firm_355() && !is_firm_355DEX() && !is_firm_421() && !is_firm_421DEX() && !is_firm_430() && !is_firm_431()) 
-        return 0;
+    u64 addr = peekq(0x80000000000004f0ULL);
 
-    u64 addr = peekq((SYSCALL_BASE + 36 * 8));
-    addr = peekq(addr);
-    
-    if(SYSCALL_BASE == SYSCALL_BASE_341 && peekq(0x8000000000017CD0ULL)!=0x4E8000203C608001ULL) return 1;
-    if(peekq(addr - 0x20) == 0x534B313000000000ULL) //SK10 HEADER
+    if((addr>>32) == 0x534B3145) {
+        addr&= 0xffffffff;
+        if(addr && peekq(0x80000000000004f8ULL)) {
+            restore_syscall8[0]= peekq(0x80000000000004f8ULL); // (8*8)
+            restore_syscall8[1]= peekq(restore_syscall8[0]);
+            pokeq(restore_syscall8[0], 0x8000000000000000ULL + (u64) (addr + 0x20));
+            return 2;
+        }
+        
         return 1;
-
+    }
+   
     return 0;
 }
 
@@ -336,12 +237,15 @@ s32 main(s32 argc, const char* argv[])
     if(memcmp(mem, "/dev_hdd", 7))
         add_sys8_path_table(mem + 1024, mem);
 
+    add_sys8_path_table("/app_home", "/dev_kk");
+
     build_sys8_path_table();
 
     free(mem);
 
     }
 
+    if(restore_syscall8[0]) sys8_pokeinstr(restore_syscall8[0], restore_syscall8[1]);
 
 	return 0;
 }
